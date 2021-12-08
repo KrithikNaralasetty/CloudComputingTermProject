92e1ebc526af1dde1c52c86639095e2be5ab66fa
KrithikNaralasetty <43697905+KrithikNaralasetty@users.noreply.github.com>
2021-12-08 12:12:10 -0800

    Updating dbConnect.js to mention localhost

--- server-api/dbConnect.js
+++ server-api/dbConnect.js
@@ -2,8 +2,8 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
         connectionLimit : 10,
-        host    : 'localhost',
-        port    :   5000,
+        host    : '172.17.0.2',
+        port    :   3306,
         database: 'coen241',
         password: '12345',
         user    : 'root'
